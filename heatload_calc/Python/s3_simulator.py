e391ec207f2ebded5ec54d27528555814d76e24b
Hisashi Miura <hisashi.office@gmail.com>
2020-06-09 22:24:55 +0900

    [improve] 不要な引数・関数の削除

--- heatload_calc/Python/s3_simulator.py
+++ heatload_calc/Python/s3_simulator.py
@@ -183,7 +183,6 @@ def run_tick(theta_o_n: float, xo_n: float, n: int, ss: PreCalcParameters, c_n: 
         brm_is_n=brm_is_n.flatten(),
         brc_i_n=brc_i_n.flatten(),
         brl_is_n=np.sum(ss.brl_is_is, axis=1),
-        wsb_jstrs=np.sum(ss.wsb_js_is, axis=1),
         wsc_is_jstrs_npls=wsc_js_npls.flatten(),
         wsv_is_jstrs_npls=wsv_js_npls.flatten(),
         fot_jstrs=ss.f_mrt_hum_is_js,
