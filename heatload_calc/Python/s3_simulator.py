d31bf407e3be8de65ff67bf462defca29f67983d
Hisashi Miura <hisashi.office@gmail.com>
2020-06-09 19:00:06 +0900

    [improve] 変数 q_gen_is_n を1次元配列から[i, 1]のベクトルに変更した。

--- heatload_calc/Python/s3_simulator.py
+++ heatload_calc/Python/s3_simulator.py
@@ -120,8 +120,8 @@ def run_tick(theta_o_n: float, xo_n: float, n: int, ss: PreCalcParameters, c_n: 
     # ステップnの室iにおける人体発湿, kg/s, [i]
     x_hum_is_n = a3.get_x_hum_i_n(theta_r_is_n=c_n.theta_r_is_n.flatten(), n_hum_i_n=ss.n_hum_is_ns[:, n])
 
-    # ステップnの室iにおける内部発熱, W, [j]
-    q_gen_is_n = ss.q_gen_is_ns[:, n] + q_hum_is_n
+    # ステップnの室iにおける内部発熱, W, [j, 1]
+    q_gen_is_n = (ss.q_gen_is_ns[:, n] + q_hum_is_n).reshape(-1, 1)
 
     # ステップnの室iにおける内部発湿, kg/s, [j]
     x_gen_is_n = ss.x_gen_is_ns[:, n] + x_hum_is_n
@@ -159,12 +159,9 @@ def run_tick(theta_o_n: float, xo_n: float, n: int, ss: PreCalcParameters, c_n: 
     # ステップnの室iにおける係数BRC
     brc_i_n = ss.c_room_is / 900.0 * c_n.theta_r_is_n\
         + np.dot(ss.p_is_js, ss.h_c_js * ss.a_srf_js * (wsc_js_npls + wsv_js_npls))\
-        + (a18.get_c_air() * a18.get_rho_air() * (
-            (v_out_vent_is_ns.flatten()) * theta_o_n
-                + np.dot(ss.v_int_vent_is, c_n.theta_r_is_n).flatten()
-               )
-        + q_gen_is_n
-        + (ss.c_cap_frnt_is.flatten() / 900.0 * c_n.theta_frnt_is_n + ss.q_sol_frnt_is_ns[:, n]) / (ss.c_cap_frnt_is.flatten() / (900.0 * ss.c_frnt_is.flatten()) + 1.0)
+        + a18.get_c_air() * a18.get_rho_air() * (v_out_vent_is_ns * theta_o_n + np.dot(ss.v_int_vent_is, c_n.theta_r_is_n))\
+        + q_gen_is_n\
+        + ((ss.c_cap_frnt_is.flatten() / 900.0 * c_n.theta_frnt_is_n + ss.q_sol_frnt_is_ns[:, n]) / (ss.c_cap_frnt_is.flatten() / (900.0 * ss.c_frnt_is.flatten()) + 1.0)
         + a18.get_c_air() * a18.get_rho_air() * v_ntrl_vent_is.flatten() * theta_o_n
     ).reshape(-1, 1)
     brc_i_n = brc_i_n.flatten()
