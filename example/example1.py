aab33feb79aadfbccd91931d144317e3536aa42d
Hisashi Miura <hisashi.office@gmail.com>
2020-06-26 21:03:39 +0900

    [improve] weather データの読み込み部分を記述した。

--- example/example1.py
+++ example/example1.py
@@ -28,7 +28,18 @@ def calc_heat_load(d: Dict):
     #   (3)ステップnにおける水平面天空日射量, W/m2, [8760 * 4]
     #   (4)ステップnにおける夜間放射量, W/m2, [8760 * 4]
     #   (5)ステップnにおける外気絶対湿度, kg/kgDA, [8760 * 4]
-    theta_o_ns, i_dn_ns, i_sky_ns, r_n_ns, x_o_ns, h_sun_ns, a_sun_ns = weather.get_weather(region=region)
+    weather.make_weather(region=region, output_data_dir='data_example1')
+
+    pp = pd.read_csv('data_example1\\weather.csv', index_col=0)
+
+    theta_o_ns = pp['temperature [degree C]'].values
+    x_o_ns = pp['absolute humidity [kg/kg(DA)]'].values
+    i_dn_ns = pp['normal direct solar radiation [W/m2]'].values
+    i_sky_ns = pp['horizontal sky solar radiation [W/m2]'].values
+    r_n_ns = pp['outward radiation [W/m2]'].values
+    h_sun_ns = pp['sun altitude [rad]'].values
+    a_sun_ns = pp['sun azimuth [rad]'].values
+
 
     # スペースの読み取り
     make_house(
--- heat_load_calc/weather/weather.py
+++ heat_load_calc/weather/weather.py
@@ -5,7 +5,7 @@ from heat_load_calc.weather import region_location
 from heat_load_calc.weather import solar_position
 
 
-def get_weather(region):
+def make_weather(region: int, output_data_dir:str):
 
     # 気象データの読み込み
     #   (1)ステップnにおける外気温度, ℃, [8760 * 4]
@@ -24,14 +24,13 @@ def get_weather(region):
     h_sun_ns, a_sun_ns = solar_position.calc_solar_position(phi_loc=phi_loc, lambda_loc=lambda_loc)
 
     dd = pd.DataFrame(index=pd.date_range(start='1/1/1989', periods=365*96, freq='15min'))
-    dd['temperature [degree C]'] = theta_o_ns
-    dd['absolute humidity [kg/kg(DA)]'] = x_o_ns
+    dd['temperature [degree C]'] = theta_o_ns.round(3)
+    dd['absolute humidity [kg/kg(DA)]'] = x_o_ns.round(6)
     dd['normal direct solar radiation [W/m2]'] = i_dn_ns
     dd['horizontal sky solar radiation [W/m2]'] = i_sky_ns
     dd['outward radiation [W/m2]'] = r_n_ns
     dd['sun altitude [rad]'] = h_sun_ns
     dd['sun azimuth [rad]'] = a_sun_ns
 
-    dd.to_csv('weather.csv', encoding='utf-8')
+    dd.to_csv(output_data_dir + '\\weather.csv', encoding='utf-8')
 
-    return theta_o_ns, i_dn_ns, i_sky_ns, r_n_ns, x_o_ns, h_sun_ns, a_sun_ns
