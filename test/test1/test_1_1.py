744e42f5fe70ee755c2b0c35b138372c12ebd379
Hisashi Miura <hisashi.office@gmail.com>
2020-06-30 23:04:52 +0900

    [rename] テストファイル

---
+++ test/test1/test_1_1.py
@@ -0,0 +1,29 @@
+import unittest
+import os
+
+from heat_load_calc.core import core
+
+
+# クラスの名前は何でも良いので、　TestSurfaceHeatBalance のような形で名前を変更してください。
+class MyTestCase(unittest.TestCase):
+
+    # 関数の名前も何でも良いので、 test_xxxxx(self): の形で適当に変更してください。
+    def test_something1(self):
+
+        data_dir = str(os.path.dirname(__file__)) + '\\data'
+
+        ds, dd = core.calc(input_data_dir=data_dir, output_data_dir=data_dir)
+
+        # 1/1 0:00の外気温度があっているかどうか？
+        self.assertEqual(2.3, dd['外気温度[℃]']['1989-01-01 00:00:00'])
+
+        # 1/1 0:15の外気温度があっているかどうか？
+        self.assertEqual(2.375, dd['外気温度[℃]']['1989-01-01 00:15:00'])
+
+        # 1/1 0:00の絶対湿度があっているかどうか？
+        self.assertEqual(0.0032, dd['外気絶対湿度[kg/kg(DA)]']['1989-01-01 00:00:00'])
+
+
+if __name__ == '__main__':
+
+    unittest.main()
