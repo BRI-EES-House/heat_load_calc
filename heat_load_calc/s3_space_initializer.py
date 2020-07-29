f9d3d310c4aeece8a9c19a55a24d70e88e51df70
Hisashi Miura <hisashi.office@gmail.com>
2020-07-30 04:30:38 +0900

    [improve] 貫流応答係数の諸侯 を一括計算する。

--- heat_load_calc/s3_space_initializer.py
+++ heat_load_calc/s3_space_initializer.py
@@ -186,7 +186,17 @@ def make_house(d, input_data_dir, output_data_dir):
     # 境界jの項別公比法における項mの公比, [j, 12]
     rows_js = [list(bss2[first_idx[i]].row) for i in np.unique(gp_idxs)]
 
+    # 境界jの貫流応答係数の初項, [j]
+    phi_t0_js = np.array([
+        s3.get_area_weighted_averaged_values_one_dimension(
+            v=np.array([bs.rft0 for bs in bss2[gp_idxs == i]]),
+            a=np.array([bs.area for bs in bss2[gp_idxs == i]])
+        )
+        for i in np.unique(gp_idxs)
+    ])
 
+    # 統合された境界j*の貫流応答係数の初項, [j*]
+#    phi_t0_bdry_jstrs = np.concatenate([ib.RFT0s for ib in ibs])
 
 
 #    RFT0s = np.array([
@@ -234,6 +244,9 @@ def make_house(d, input_data_dir, output_data_dir):
 
     # 統合された境界j*の貫流応答係数の初項, [j*]
     phi_t0_bdry_jstrs = np.concatenate([ib.RFT0s for ib in ibs])
+
+    phi_t0_bdry_jstrs = phi_t0_js
+
 
     # 統合された境界j*の吸熱応答係数の初項, m2K/W, [j*]
     phi_a0_bdry_jstrs = np.concatenate([ib.RFA0s for ib in ibs])
@@ -391,7 +404,7 @@ def make_house(d, input_data_dir, output_data_dir):
             'area': a_js[i],
             'phi_a0': phi_a0_bdry_jstrs[i],
             'phi_a1': list(phi_a1_bdry_jstrs_ms[i]),
-            'phi_t0': phi_t0_bdry_jstrs[i],
+            'phi_t0': phi_t0_js[i],
             'phi_t1': list(phi_t1_bdry_jstrs_ms[i]),
             'r': rows_js[i],
             'h_i': h_i_js[i],
