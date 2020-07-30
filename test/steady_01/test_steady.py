915f7110b4f2d08787fbfb615d51f6035a1982ac
Serikawa <serikawa@satoh-er.com>
2020-07-30 19:04:46 +0900

    定常ケース01テスト 実行中に停止

---
+++ test/steady_01/test_steady.py
@@ -0,0 +1,41 @@
+import json
+import os
+import time
+import unittest
+
+from heat_load_calc import s3_space_initializer
+from heat_load_calc.weather import weather
+from heat_load_calc.core import core
+
+
+# 定常状態のテスト
+class TestSteadyState(unittest.TestCase):
+
+    # @unittest.skip('時間がかかるのでskip')
+    def test_case_01(self):
+
+        s_folder = 'data'
+
+        bln_make_weather = True
+
+        bln_space_initialize = True
+
+        js = open(str(os.path.dirname(__file__)) + '/' + s_folder + '/input_residential.json', 'r', encoding='utf-8')
+
+        d = json.load(js)
+
+        if bln_make_weather:
+            weather.make_weather(region=d['common']['region'], output_data_dir=s_folder, csv_output=True)
+
+        if bln_space_initialize:
+            s3_space_initializer.make_house(d=d, input_data_dir=s_folder, output_data_dir=s_folder)
+
+        data_dir = str(os.path.dirname(__file__)) + '/' + s_folder
+
+        ds, dd = core.calc(input_data_dir=data_dir, output_data_dir=data_dir,
+                           show_simple_result=True, show_detail_result=True)
+
+        # 対流熱伝達率を放射熱伝達率のユニットテストより求め、室温を計算する必要がある
+        # self.assertAlmostEqual(0.0, dd['main_occupant_room_空気温度[℃]']['1989-12-31 00:00:00'])
+
+
