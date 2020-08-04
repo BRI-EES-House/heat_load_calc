f6e686403789245fe5ddaa4c96c8ea702521efef
Hisashi Miura <hisashi.office@gmail.com>
2020-08-05 00:22:11 +0900

    [update] テストイメージ追加

--- test/test1/test_1_1.py
+++ test/test1/test_1_1.py
@@ -1,5 +1,6 @@
 import unittest
 import os
+import json
 
 from heat_load_calc.core import core
 
@@ -17,21 +18,36 @@ class MyTestCase(unittest.TestCase):
         cls._ds = ds
         cls._dd = dd
 
+        with open(data_dir + '/mid_data_house.json') as f:
+            mdh = json.load(f)
+
+        cls._mdh = mdh
 
 #    @unittest.skip('時間がかかるのでとりあえずskip')
     def test_weather(self):
 
-        ds = self._ds
-        dd = self._dd
-
         # 1/1 0:00の外気温度があっているかどうか？
-        self.assertEqual(2.3, dd['out_temp']['1989-01-01 00:00:00'])
+        self.assertEqual(2.3, self._dd['out_temp']['1989-01-01 00:00:00'])
 
         # 1/1 0:15の外気温度があっているかどうか？
-        self.assertEqual(2.375, dd['out_temp']['1989-01-01 00:15:00'])
+        self.assertEqual(2.375, self._dd['out_temp']['1989-01-01 00:15:00'])
 
         # 1/1 0:00の絶対湿度があっているかどうか？
-        self.assertEqual(0.0032, dd['out_abs_humid']['1989-01-01 00:00:00'])
+        self.assertEqual(0.0032, self._dd['out_abs_humid']['1989-01-01 00:00:00'])
+
+    def test_temp_change(self):
+
+        t_r_old = self._dd['rm0_t_r']['1989-01-01 00:00:00']
+        t_r_new = self._dd['rm0_t_r']['1989-01-01 00:15:00']
+
+        volume = self._mdh['spaces'][0]['volume']  # m3
+        c_air = 1005  # J/kg K
+        rho_air = 1.2  # kg/m3
+
+        heat = (t_r_new - t_r_old) * volume * c_air * rho_air
+
+        self.assertAlmostEqual(-54808.782520573935, heat)
+
 
 
 if __name__ == '__main__':
