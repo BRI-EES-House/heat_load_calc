0daf377eaf7c5e2f8959a2eed0459b4ccb05b93a
Serikawa <serikawa@satoh-er.com>
2020-08-27 15:16:32 +0900

    作成した中間ファイルを使用するか指定するようにした

--- test/test_singlezone/steady_01/test_steady.py
+++ test/test_singlezone/steady_01/test_steady.py
@@ -1,6 +1,6 @@
 import json
 import os
-import time
+import shutil
 import unittest
 
 from heat_load_calc import s3_space_initializer
@@ -14,30 +14,55 @@ class TestSteadyState(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
 
-        s_folder = 'data'
+        # 計算用フォルダ
+        s_folder = str(os.path.dirname(__file__)) + '/data'
 
-        bln_make_weather = False
+        # ファイル一時保管フォルダ
+        s_folder_temporary = str(os.path.dirname(__file__)) + '/data_temporary'
 
-        bln_space_initialize = False
+        # ファイル一時保管フォルダ作成
+        os.mkdir(s_folder_temporary)
 
-        js = open(str(os.path.dirname(__file__)) + '/' + s_folder + '/input_residential.json', 'r', encoding='utf-8')
-
+        # 計算条件読込
+        js = open(s_folder + '/input_residential.json', 'r', encoding='utf-8')
         d = json.load(js)
 
-        if bln_make_weather:
-            weather.make_weather(region=d['common']['region'], output_data_dir=s_folder, csv_output=True)
+        # 気象データ作成
+        weather.make_weather(region=d['common']['region'], output_data_dir=s_folder_temporary, csv_output=True)
 
-        if bln_space_initialize:
-            s3_space_initializer.make_house(d=d, input_data_dir=s_folder, output_data_dir=s_folder)
+        # 中間データ作成
+        s3_space_initializer.make_house(d=d, input_data_dir=s_folder_temporary, output_data_dir=s_folder_temporary)
 
-        data_dir = str(os.path.dirname(__file__)) + '/' + s_folder
+        # 使用するデータファイルの指定
+        d_copy = {
+            'weather.csv': False,
+            'mid_data_house.json': True,
+            'mid_data_theta_o_sol.csv': False,
+            'mid_data_q_trs_sol.csv': False,
+            'mid_data_local_vent.csv': False,
+            'mid_data_occupants.csv': False,
+            'mid_data_heat_generation.csv': False,
+            'mid_data_moisture_generation.csv': False,
+            'mid_data_ac_demand.csv': False
+        }
 
-        ds, dd = core.calc(input_data_dir=data_dir, output_data_dir=data_dir,
+        # 指定されたデータファイルのコピー
+        for s_file in d_copy.keys():
+            if d_copy[s_file]:
+                shutil.copy(s_folder_temporary + '/' + s_file, s_folder + '/' + s_file)
+
+        # ファイル一時保管フォルダ削除
+        shutil.rmtree(s_folder_temporary)
+
+        # 計算実行
+        ds, dd = core.calc(input_data_dir=s_folder, output_data_dir=s_folder,
                            show_simple_result=True, show_detail_result=True)
 
+        # 計算結果格納
         cls._ds = ds
         cls._dd = dd
 
+    # 室空気温[℃]のテスト
     def test_case_01_room_temp(self):
 
         # 対流熱伝達率を放射熱伝達率のユニットテストより求め、室温を計算する必要がある
@@ -45,14 +70,12 @@ class TestSteadyState(unittest.TestCase):
         hc = self._dd['rm0_b0_hic_s']['1989-12-31 00:00:00']
         self.assertAlmostEqual(0.0 + 100/(6/(1/hc + 0.075 + 0.04)), self._dd['rm0_t_r']['1989-12-31 00:00:00'])
 
+    # 室内側表面熱流[W/m2]のテスト
     def test_case_01_heat_flow(self):
 
-        # 室内側表面熱流, W/m2 のテスト
         self.assertAlmostEqual(100/6, self._dd['rm0_b0_qic_s']['1989-12-31 00:00:00'])
 
+    # 表面温度[℃]のテスト
     def test_case_01_surface_temp(self):
 
-        # 表面温度, C のテスト
         self.assertAlmostEqual(0.0 + 100/6*(0.075 + 0.04), self._dd['rm0_b0_t_s']['1989-12-31 00:00:00'])
-
-
