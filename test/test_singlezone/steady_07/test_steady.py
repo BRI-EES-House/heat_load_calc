eda621fd29a5460018a15ff9c359ff6dcc6421cd
Serikawa <serikawa@satoh-er.com>
2020-09-04 17:22:44 +0900

    make_house_for_test関数使用

--- test/test_singlezone/steady_07/test_steady.py
+++ test/test_singlezone/steady_07/test_steady.py
@@ -20,46 +20,15 @@ class TestSteadyState(unittest.TestCase):
         # 計算用フォルダ
         s_folder = str(os.path.dirname(__file__)) + '/data'
 
-        # ファイル一時保管フォルダ
-        s_folder_temporary = str(os.path.dirname(__file__)) + '/data_temporary'
-
-        # ファイル一時保管フォルダ作成
-        os.mkdir(s_folder_temporary)
-
         # 計算条件読込
         js = open(s_folder + '/input_residential.json', 'r', encoding='utf-8')
         d = json.load(js)
 
-        # 気象データ作成
-        weather.make_weather(region=d['common']['region'], output_data_dir=s_folder_temporary, csv_output=True)
-
-        # 中間データ作成
-        s3_space_initializer.make_house(d=d, input_data_dir=s_folder_temporary, output_data_dir=s_folder_temporary)
-
-        # 使用するデータファイルの指定
-        d_copy = {
-            'weather.csv': False,
-            'mid_data_house.json': True,
-            'mid_data_theta_o_sol.csv': False,
-            'mid_data_q_trs_sol.csv': False,
-            'mid_data_local_vent.csv': False,
-            'mid_data_occupants.csv': False,
-            'mid_data_heat_generation.csv': False,
-            'mid_data_moisture_generation.csv': False,
-            'mid_data_ac_demand.csv': False
-        }
-
-        # 指定されたデータファイルのコピー
-        for s_file in d_copy.keys():
-            if d_copy[s_file]:
-                shutil.copy(s_folder_temporary + '/' + s_file, s_folder + '/' + s_file)
-
-        # ファイル一時保管フォルダ削除
-        shutil.rmtree(s_folder_temporary)
+        # 中間データ作成（建物のみ）
+        s3_space_initializer.make_house_for_test(d=d, input_data_dir=s_folder, output_data_dir=s_folder)
 
         # 計算実行
-        ds, dd = core.calc(input_data_dir=s_folder, output_data_dir=s_folder,
-                           show_simple_result=True, show_detail_result=True)
+        ds, dd = core.calc(input_data_dir=s_folder, output_data_dir=s_folder)
 
         # 計算結果格納
         cls._ds = ds
