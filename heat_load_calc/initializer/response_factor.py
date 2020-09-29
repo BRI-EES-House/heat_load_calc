4a36e5ec557e7242cb9fef08a3e210f83437d2c4
SATOH-VAIO-S13\satoh <satoh@satoh-er.com>
2020-09-29 13:32:49 +0900

    土壌の応答係数計算には屋外側の表面熱伝達抵抗は不要なため削除した

--- heat_load_calc/initializer/response_factor.py
+++ heat_load_calc/initializer/response_factor.py
@@ -177,10 +177,10 @@ def get_step_reps_of_wall(C_i_k_p, R_i_k_p, laps: List[float], alp: List[float],
             matF[lngI, lngJ] = laps / (laps + root)
 
     # 最小二乗法のための係数行列を作成
-    # matU = np.zeros((self.__Nroot, self.__Nroot))
-    # for lngK in range(self.__Nroot):
-    #    for lngJ in range(self.__Nroot):
-    #        matU[lngK, lngJ] = np.sum([matF[:, lngK] * matF[:, lngJ]])
+    # matU = np.zeros((nroot, nroot))
+    # for lngK in range(nroot):
+    #    for lngJ in range(nroot):
+    #         matU[lngK, lngJ] = np.sum([matF[:, lngK] * matF[:, lngJ]])
     matU = np.dot(matF.T, matF)
 
     # 最小二乗法のための定数項行列を作成
@@ -374,7 +374,7 @@ class ResponseFactorFactoryTransientEnvelope(ResponseFactorFactory):
 
 class ResponseFactorFactoryTransientGround(ResponseFactorFactory):
 
-    def __init__(self, cs: List[float], rs: List[float], r_o):
+    def __init__(self, cs: List[float], rs: List[float]):
         """
 
         Args:
@@ -386,7 +386,6 @@ class ResponseFactorFactoryTransientGround(ResponseFactorFactory):
         super().__init__()
         self._cs = cs
         self._rs = rs
-        self._r_o = r_o
 
     def get_response_factors(self) -> ResponseFactor:
 
