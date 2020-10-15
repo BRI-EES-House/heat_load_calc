136d1dab70115ec7ee1fdd0194efb5653c6a0c2e
Serikawa <serikawa@satoh-er.com>
2020-10-15 17:33:36 +0900

    正解計算用Excelに放射熱伝達率の計算を追加

--- test/test_singlezone/steady_06/test_steady06.py
+++ test/test_singlezone/steady_06/test_steady06.py
@@ -35,7 +35,7 @@ class TestSteadyState(unittest.TestCase):
         s3_space_initializer.make_house_for_test(d=d, input_data_dir=s_folder, output_data_dir=s_folder)
 
         # 計算実行
-        ds, dd = core.calc(input_data_dir=s_folder, output_data_dir=s_folder, show_detail_result=True)
+        ds, dd = core.calc(input_data_dir=s_folder, output_data_dir=s_folder, show_detail_result=False)
 
         # 計算結果格納
         cls._ds = ds
@@ -44,18 +44,18 @@ class TestSteadyState(unittest.TestCase):
     # 室空気温[℃]のテスト
     def test_room_temp(self):
 
-        self.assertAlmostEqual(4.57208812799, self._dd['rm0_t_r']['1989-12-31 00:00:00'])
+        self.assertAlmostEqual(4.57208812325315, self._dd['rm0_t_r']['1989-12-31 00:00:00'])
 
     # 室内側表面熱流[W/m2]のテスト
     def test_heat_flow(self):
 
-        self.assertAlmostEqual(12.7809215858034, self._dd['rm0_b1_qiall_s']['1989-12-31 00:00:00'])
-        self.assertAlmostEqual(36.6603790546126, self._dd['rm0_b4_qiall_s']['1989-12-31 00:00:00'])
-        self.assertAlmostEqual(12.2159346101681, self._dd['rm0_b5_qiall_s']['1989-12-31 00:00:00'])
+        self.assertAlmostEqual(12.7809215844841, self._dd['rm0_b1_qiall_s']['1989-12-31 00:00:00'])
+        self.assertAlmostEqual(36.6603790532707, self._dd['rm0_b4_qiall_s']['1989-12-31 00:00:00'])
+        self.assertAlmostEqual(12.2159346088262, self._dd['rm0_b5_qiall_s']['1989-12-31 00:00:00'])
 
     # 表面温度[℃]のテスト
     def test_surface_temp(self):
 
-        self.assertAlmostEqual(1.34268391264, self._dd['rm0_b1_t_s']['1989-12-31 00:00:00'])
-        self.assertAlmostEqual(4.21594359107, self._dd['rm0_b4_t_s']['1989-12-31 00:00:00'])
-        self.assertAlmostEqual(1.40483247996, self._dd['rm0_b5_t_s']['1989-12-31 00:00:00'])
+        self.assertAlmostEqual(1.34268391269238, self._dd['rm0_b1_t_s']['1989-12-31 00:00:00'])
+        self.assertAlmostEqual(4.21594359112589, self._dd['rm0_b4_t_s']['1989-12-31 00:00:00'])
+        self.assertAlmostEqual(1.40483248001477, self._dd['rm0_b5_t_s']['1989-12-31 00:00:00'])
