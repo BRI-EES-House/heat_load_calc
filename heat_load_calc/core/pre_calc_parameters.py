691b3224235f942a8cd0770a9a059880b25ca712
Hisashi Miura <hisashi.office@gmail.com>
2021-06-10 00:59:49 +0900

    変数名・単位間違い等の微修正

--- heat_load_calc/core/pre_calc_parameters.py
+++ heat_load_calc/core/pre_calc_parameters.py
@@ -254,7 +254,7 @@ def make_pre_calc_parameters(delta_t: float, data_directory: str) -> (PreCalcPar
             ([next_vent['upstream_room_id']], next_vent['volume']) for next_vent in s['ventilation']['next_spaces']
         ] for s in ss
     ]
-    v_int_vent_is_is = _get_v_int_vent_is(next_vents)
+    v_int_vent_is_is = _get_v_int_vent_is_is(next_vents)
 
     # 室iの自然風利用時の換気量, m3/s, [i, 1]
     v_ntrl_vent_is = np.array([s['ventilation']['natural'] for s in ss]).reshape(-1, 1)
@@ -705,7 +705,7 @@ def _get_responsfactors(bs):
     return phi_a0_js, phi_a1_js_ms, phi_t0_js, phi_t1_js_ms, r_js_ms
 
 
-def _get_v_int_vent_is(next_vents: List[List[Tuple]]) -> np.ndarray:
+def _get_v_int_vent_is_is(next_vents: List[List[Tuple]]) -> np.ndarray:
     """
     隣室iから室iへの機械換気量マトリクスを生成する。
     Args:
@@ -713,7 +713,7 @@ def _get_v_int_vent_is(next_vents: List[List[Tuple]]) -> np.ndarray:
                         2重のリスト構造を持つ。
                         外側のリスト：室、（下流側の室を基準とする。）
                         内側のリスト：換気経路（数は任意であり、換気経路が無い（0: 空のリスト）場合もある。）
-                        変数はタプル （上流側の室ID: int, 換気量（m3/h): float)
+                        変数はタプル （上流側の室ID: int, 換気量（m3/s): float)
     Returns:
         隣室iから室iへの機械換気量マトリクス, m3/s, [i, i]
             例えば、
@@ -732,7 +732,7 @@ def _get_v_int_vent_is(next_vents: List[List[Tuple]]) -> np.ndarray:
     n_rooms = len(next_vents)
 
     # 隣室iから室iへの換気量マトリックス, m3/s [i, i]
-    v_int_vent_is = np.zeros((n_rooms, n_rooms), dtype=float)
+    v_int_vent_is_is = np.zeros((n_rooms, n_rooms), dtype=float)
 
     # 室iのループ（風下室ループ）
     for i, next_vent_is in enumerate(next_vents):
@@ -741,10 +741,9 @@ def _get_v_int_vent_is(next_vents: List[List[Tuple]]) -> np.ndarray:
         # 取得するのは、(ID: int, 換気量(m3/h): float) のタプル
         for (idx, volume) in next_vent_is:
 
-            # m3/hからm3/sへの単位変換を行っている
             # 風上側
             if i != idx:
-                v_int_vent_is[i, idx] += volume
+                v_int_vent_is_is[i, idx] += volume
 
-    return v_int_vent_is
+    return v_int_vent_is_is
 
