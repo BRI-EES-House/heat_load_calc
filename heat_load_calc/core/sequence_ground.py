beb4f300ed3a8903ea12b8c018a2c3a2a46f16c4
SATOH-VAIO-S13\satoh <satoh@satoh-er.com>
2021-11-05 20:17:48 +0900

    地盤の計算のステップを見直した

--- heat_load_calc/core/sequence_ground.py
+++ heat_load_calc/core/sequence_ground.py
@@ -4,7 +4,7 @@ from heat_load_calc.core.pre_calc_parameters import PreCalcParametersGround
 from heat_load_calc.core.conditions import GroundConditions
 
 
-# 地盤の計算
+# 地盤の計算（n+1ステップを計算する）
 def run_tick(gc_n: GroundConditions, ss: PreCalcParametersGround, n: int):
 
     h_i_js = ss.h_s_r_js + ss.h_s_c_js
@@ -12,13 +12,12 @@ def run_tick(gc_n: GroundConditions, ss: PreCalcParametersGround, n: int):
     theta_dsh_srf_a_js_ms_npls = ss.phi_a1_js_ms * gc_n.q_srf_js_n + ss.r_js_ms * gc_n.theta_dsh_srf_a_js_ms_n
     theta_dsh_srf_t_js_ms_npls = ss.phi_t1_js_ms * ss.theta_dstrb_js_ns[:, [n]] + ss.r_js_ms * gc_n.theta_dsh_srf_t_js_ms_n
 
-    theta_s_js_npls = (ss.phi_a0_js * h_i_js * ss.theta_o_ns[n] + ss.phi_t0_js * ss.theta_dstrb_js_ns[:, [n+1]]
+    theta_s_js_npls = (ss.phi_a0_js * h_i_js * ss.theta_o_ns[n+1] + ss.phi_t0_js * ss.theta_dstrb_js_ns[:, [n+1]]
         + np.sum(theta_dsh_srf_a_js_ms_npls, axis=1, keepdims=True)
         + np.sum(theta_dsh_srf_t_js_ms_npls, axis=1, keepdims=True)) \
         / (1.0 + ss.phi_a0_js * h_i_js)
 
-    # TODO: ここの外気温度は n+1 を使用する必要があるのではないか。
-    q_srf_js_n = h_i_js * (ss.theta_o_ns[n] - theta_s_js_npls)
+    q_srf_js_n = h_i_js * (ss.theta_o_ns[n+1] - theta_s_js_npls)
 
     return GroundConditions(
         theta_dsh_srf_a_js_ms_n=theta_dsh_srf_a_js_ms_npls,
