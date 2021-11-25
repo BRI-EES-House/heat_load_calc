35d022d9be814d570e711d0663c3535668520cdd
SATOH-VAIO-S13\satoh <satoh@satoh-er.com>
2021-11-25 11:59:10 +0900

    テストが通るように修正

--- test/test_all_at_one_with_ground/test_singleroom_with_ground.py
+++ test/test_all_at_one_with_ground/test_singleroom_with_ground.py
@@ -37,25 +37,26 @@ class TestSigleRoomWithFround(unittest.TestCase):
         # initializer.make_house(d=d, input_data_dir=s_folder, output_data_dir=s_folder)
 
         # 計算実行
-        dd = core.calc(input_data_dir=s_folder, output_data_dir=s_folder,
+        dd_i, dd_a = core.calc(input_data_dir=s_folder, output_data_dir=s_folder,
                            show_detail_result=False, n_d_main=30, n_d_run_up=10, n_d_run_up_build=0)
 
         # 計算結果格納
-        cls._dd = dd
+        cls._dd_i = dd_i
+        cls._dd_a = dd_a
 
     # 室空気温[℃]のテスト
     def test_case_01_room_temp(self):
 
-        self.assertAlmostEqual(10.0057217473846, self._dd['rm0_t_r']['1989-1-30 23:45:00'])
+        self.assertAlmostEqual(10.0057217473846, self._dd_i['rm0_t_r']['1989-1-30 23:45:00'])
 
     # 室内側表面熱流[W/m2]のテスト
     def test_case_01_heat_flow(self):
 
-        self.assertAlmostEqual(-0.000471298032647407, self._dd['rm0_b0_qiall_s']['1989-1-30 23:45:00'])
-        self.assertAlmostEqual(-0.000471298032647407, self._dd['rm0_b1_qiall_s']['1989-1-30 23:45:00'])
+        self.assertAlmostEqual(-0.000471298032647407, self._dd_i['rm0_b0_qiall_s']['1989-1-30 23:45:00'])
+        self.assertAlmostEqual(-0.000471298032647407, self._dd_i['rm0_b1_qiall_s']['1989-1-30 23:45:00'])
 
     # 表面温度[℃]のテスト
     def test_case_01_surface_temp(self):
 
-        self.assertAlmostEqual(10.0058767197643, self._dd['rm0_b0_t_s']['1989-1-30 23:45:00'])
-        self.assertAlmostEqual(10.0058767197643, self._dd['rm0_b1_t_s']['1989-1-30 23:45:00'])
+        self.assertAlmostEqual(10.0058767197643, self._dd_i['rm0_b0_t_s']['1989-1-30 23:45:00'])
+        self.assertAlmostEqual(10.0058767197643, self._dd_i['rm0_b1_t_s']['1989-1-30 23:45:00'])
