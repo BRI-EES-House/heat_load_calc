1757f05035b55ae32e1a15ca49f8d97e3954a574
Hisashi <hisashi.office@gmail.com>
2022-04-20 18:01:51 +0900

    ロギング項目のうちの平均値・積算値についても変数を整理した。

--- src/heat_load_calc/core/sequence.py
+++ src/heat_load_calc/core/sequence.py
@@ -430,13 +430,13 @@ def run_tick(n: int, delta_t: float, ss: PreCalcParameters, c_n: Conditions, log
         logger.x_r_is_ns[:, n_i] = x_r_is_n_pls.flatten()
         logger.theta_frt_is_ns[:, n_i] = theta_frt_is_n_pls.flatten()
         logger.x_frt_is_ns[:, n_i] = x_frt_is_n_pls.flatten()
-        logger.theta_ei[:, n_i] = theta_ei_js_n_pls.flatten()
+        logger.theta_ei_js_ns[:, n_i] = theta_ei_js_n_pls.flatten()
 
         # 次の時刻に引き渡さない値
         logger.theta_ot[:, n_i] = theta_ot_is_n_pls.flatten()
-        logger.theta_s[:, n_i] = theta_s_js_n_pls.flatten()
-        logger.theta_rear[:, n_i] = theta_rear_js_n.flatten()
-        logger.qiall_s[:, n_i] = q_s_js_n_pls.flatten()
+        logger.theta_s_js_ns[:, n_i] = theta_s_js_n_pls.flatten()
+        logger.theta_rear_js_ns[:, n_i] = theta_rear_js_n.flatten()
+        logger.q_i_all_s_ns_js[:, n_i] = q_s_js_n_pls.flatten()
 #        logger.pmv_is_ns[:, n_i] = pmv_is_n_pls.flatten()
 #        logger.ppd_is_ns[:, n_i] = ppd_is_n_pls.flatten()
 
@@ -448,22 +448,22 @@ def run_tick(n: int, delta_t: float, ss: PreCalcParameters, c_n: Conditions, log
         n_a = n
 
         # 次の時刻に引き渡す値
-        logger.operation_mode[:, n_a] = operation_mode_is_n.flatten()
+        logger.operation_mode_is_ns[:, n_a] = operation_mode_is_n.flatten()
 
         # 次の時刻に引き渡さない値
         # 積算値
-        logger.l_cs[:, n_a] = l_cs_is_n.flatten()
-        logger.l_rs[:, n_a] = l_rs_is_n.flatten()
-        logger.l_cl[:, n_a] = l_cl_is_n.flatten()
+        logger.l_cs_is_ns[:, n_a] = l_cs_is_n.flatten()
+        logger.l_rs_is_ns[:, n_a] = l_rs_is_n.flatten()
+        logger.l_cl_is_ns[:, n_a] = l_cl_is_n.flatten()
         # 平均値
-        logger.h_hum_c_is_n[:, n_a] = h_hum_c_is_n.flatten()
-        logger.h_hum_r_is_n[:, n_a] = h_hum_r_is_n.flatten()
-        logger.q_hum[:, n_a] = q_hum_is_n.flatten()
-        logger.x_hum[:, n_a] = x_hum_is_n.flatten()
+        logger.h_hum_c_is_ns[:, n_a] = h_hum_c_is_n.flatten()
+        logger.h_hum_r_is_ns[:, n_a] = h_hum_r_is_n.flatten()
+        logger.q_hum_is_ns[:, n_a] = q_hum_is_n.flatten()
+        logger.x_hum_is_ns[:, n_a] = x_hum_is_n.flatten()
         logger.v_reak_is_ns[:, n_a] = v_leak_is_n.flatten()
         logger.v_ntrl_is_ns[:, n_a] = v_vent_ntr_is_n.flatten()
-        logger.v_hum[:, n_a] = v_hum_is_n.flatten()
-        logger.clo[:, n_a] = clo_is_n.flatten()
+        logger.v_hum_is_ns[:, n_a] = v_hum_is_n.flatten()
+        logger.clo_is_ns[:, n_a] = clo_is_n.flatten()
 
     return Conditions(
         operation_mode_is_n=operation_mode_is_n,
