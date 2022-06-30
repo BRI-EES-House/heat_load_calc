37e206757da316e125667a8849fbaee2ed8e2727
Hisashi <hisashi.office@gmail.com>
2022-06-30 12:03:39 +0900

    building.py を作成

---
+++ src/test/module_test/test_building.py
@@ -0,0 +1,22 @@
+﻿import unittest
+import logging
+
+from heat_load_calc import building
+
+
+@unittest.skip("作成中")
+class TestBuilding(unittest.TestCase):
+
+    def test_use_default_value(self):
+
+        building.Building.create_building(
+            d={
+                'infiltration': {
+                    'method': 'error',
+                    'story': 1,
+                    'c_value_estimate': 'specify',
+                    'c_value': 1.0,
+                    'inside_pressure': 'negative'
+                }
+            }
+        )
