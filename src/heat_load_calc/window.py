710ead4aa42d0911313d377727091a8982cc7941
Hisashi <hisashi.office@gmail.com>
2023-02-11 14:39:42 +0900

    eq.29, table2, eq.30,31, table3, 4

--- src/heat_load_calc/window.py
+++ src/heat_load_calc/window.py
@@ -46,28 +46,24 @@ class Window:
             flame_type: _description_. Defaults to FlameType.MIXED.
         """
 
-        self._glass_type = glass_type
-
-        self._u_w_f_j = self._get_u_w_f_j(flame_type=flame_type)
-        self._r_a_w_g_j = self._get_r_a_w_g_j(r_a_w_g_j=r_a_w_g_j, flame_type=flame_type)
-        self._u_w_g_j = self._get_u_w_g_j(u_w_j=u_w_j, u_w_f_j=self._u_w_f_j, r_a_w_g_j=self._r_a_w_g_j)
-        self._eta_w_g_j = self._get_eta_w_g_j(eta_w_j=eta_w_j, r_a_w_g_j=self._r_a_w_g_j)
-        r_w_o_w, r_w_i_w, r_w_o_s, r_w_i_s = self._get_r_w()
-        self._u_w_g_s_j = self._get_u_w_g_s_j(u_w_g_j=self._u_w_g_j, r_w_o_w=r_w_o_w, r_w_i_w=r_w_i_w, r_w_o_s=r_w_o_s,
-                                              r_w_i_s=r_w_i_s)
-
+        u_w_f_j = _get_u_w_f_j(flame_type=flame_type)
+        r_a_w_g_j = _get_r_a_w_g_j(r_a_w_g_j=r_a_w_g_j, flame_type=flame_type)
+        u_w_g_j = _get_u_w_g_j(u_w_j=u_w_j, u_w_f_j=u_w_f_j, r_a_w_g_j=r_a_w_g_j)
+        eta_w_g_j = _get_eta_w_g_j(eta_w_j=eta_w_j, r_a_w_g_j=r_a_w_g_j)
+        r_w_o_w, r_w_i_w, r_w_o_s, r_w_i_s = _get_r_w()
+        u_w_g_s_j = _get_u_w_g_s_j(u_w_g_j=u_w_g_j, r_w_o_w=r_w_o_w, r_w_i_w=r_w_i_w, r_w_o_s=r_w_o_s, r_w_i_s=r_w_i_s)
         r_r_w_g_j = _get_r_r_w_g_j(
-            u_w_g_j=self._u_w_g_j,
-            u_w_g_s_j=self._u_w_g_s_j,
+            u_w_g_j=u_w_g_j,
+            u_w_g_s_j=u_w_g_s_j,
             r_w_o_w=r_w_o_w,
             r_w_i_w=r_w_i_w,
             r_w_o_s=r_w_o_s,
             glass_type=glass_type
         )
-        rho_w_g_s1f_j = _get_rho_w_g_s1f_j(r_r_w_g_j=r_r_w_g_j, eta_w_g_j=self._eta_w_g_j)
+        rho_w_g_s1f_j = _get_rho_w_g_s1f_j(r_r_w_g_j=r_r_w_g_j, eta_w_g_j=eta_w_g_j)
         rho_w_g_s2f_j = _get_rho_w_g_s2f_j(glass_type=glass_type)
         tau_w_g_j = _get_tau_w_g_j(
-            eta_w_g_j=self._eta_w_g_j,
+            eta_w_g_j=eta_w_g_j,
             r_r_w_g_j=r_r_w_g_j,
             rho_w_g_s1f_j=rho_w_g_s1f_j,
             rho_w_g_s2f_j=rho_w_g_s2f_j,
@@ -77,6 +73,12 @@ class Window:
         tau_w_g_s2_j = _get_tau_w_g_s2_j(tau_w_g_s1_j=tau_w_g_s1_j, glass_type=glass_type)
         rho_w_g_s1b_j = _get_rho_w_g_s1b_j(tau_w_g_s1_j=tau_w_g_s1_j, glass_type=glass_type)
 
+        self._glass_type = glass_type
+        self._u_w_f_j = u_w_f_j
+        self._r_a_w_g_j = r_a_w_g_j
+        self._u_w_g_j = u_w_g_j
+        self._eta_w_g_j = eta_w_g_j
+        self._u_w_g_s_j = u_w_g_s_j
         self._r_r_w_g_j = r_r_w_g_j
         self._rho_w_g_s1f_j = rho_w_g_s1f_j
         self._rho_w_g_s2f_j = rho_w_g_s2f_j
@@ -355,106 +357,6 @@ class Window:
         """
         return self._tau_w_g_j
 
-    @staticmethod
-    def _get_u_w_f_j(flame_type: FlameType) -> float:
-        """建具部分の熱損失係数（U値）を取得する。
-
-        Args:
-            flame_type: 建具（フレーム）材質の種類
-
-        Returns:
-            境界 j の窓の建具部分の熱損失係数（U値）, W/m2K
-        """
-
-        return {
-            FlameType.RESIN: 2.2,
-            FlameType.WOOD: 2.2,
-            FlameType.ALUMINUM: 6.6,
-            FlameType.MIXED_WOOD: 4.7,
-            FlameType.MIXED_RESIN: 4.7
-        }[flame_type]
-
-    @staticmethod
-    def _get_r_a_w_g_j(r_a_w_g_j: Optional[float], flame_type: FlameType) -> float:
-        """窓の面積に対するグレージングの面積の比が指定されていない場合に枠（フレーム）材質の種類に応じてデフォルト値を定める。
-
-        Args:
-            r_a_w_g_j: 境界 j の窓の面積に対するグレージングの面積の比, -
-            flame_type: 建具（フレーム）材質の種類
-        Returns:
-            境界 j の窓の面積に対するグレージングの面積の比, -
-        """
-
-        if r_a_w_g_j is None:
-            return {
-                FlameType.RESIN: 0.72,
-                FlameType.WOOD: 0.72,
-                FlameType.ALUMINUM: 0.8,
-                FlameType.MIXED_WOOD: 0.8,
-                FlameType.MIXED_RESIN: 0.8
-            }[flame_type]
-        else:
-            return r_a_w_g_j
-
-    @staticmethod
-    def _get_u_w_g_j(u_w_j: float, u_w_f_j: float, r_a_w_g_j: float) -> float:
-        """窓のガラス部分の熱損失係数（U値）を取得する。
-
-        Args:
-            u_w_j: 境界 j の窓の熱損失係数, W/m2K
-            u_w_f_j: 境界 j の窓の建具部分の熱損失係数（U値）, W/m2K
-            r_a_w_g_j: 境界 j の窓の面積に対するグレージングの面積の比, -
-
-        Returns:
-            境界 j の窓のガラス部分の熱損失係数（U値）, W/m2K
-        """
-        return (u_w_j - u_w_f_j * (1 - r_a_w_g_j)) / r_a_w_g_j
-
-    @staticmethod
-    def _get_eta_w_g_j(eta_w_j: float, r_a_w_g_j: float) -> float:
-        """_summary_
-
-        Args:
-            eta_w_j: 境界 j の窓の日射熱取得率
-            r_a_w_g_j: 境界 j の窓の面積に対するグレージングの面積の比, -
-
-        Returns:
-            境界 j の窓のガラス部分の日射熱取得率, -
-        """
-        return eta_w_j / r_a_w_g_j
-
-    @staticmethod
-    def _get_r_w() -> Tuple[float, float, float, float]:
-        """窓の表面熱伝達抵抗を求める。
-
-        Returns:
-            窓の室外側表面熱伝達抵抗（冬季条件）, m2K/W
-            窓の室内側表面熱伝達抵抗（冬季条件）, m2K/W
-            窓の室外側表面熱伝達抵抗（夏季条件）, m2K/W
-            窓の室内側表面熱伝達抵抗（夏季条件）, m2K/W
-        """
-        r_w_o_w = 0.0415
-        r_w_i_w = 0.1228
-        r_w_o_s = 0.0756
-        r_w_i_s = 0.1317
-        return r_w_o_w, r_w_i_w, r_w_o_s, r_w_i_s
-
-    @staticmethod
-    def _get_u_w_g_s_j(u_w_g_j: float, r_w_o_w: float, r_w_i_w: float, r_w_o_s: float, r_w_i_s: float) -> float:
-        """窓のガラス部分の熱損失係数（夏期条件）を計算する。
-
-        Args:
-            u_w_g_j: 境界jの窓のガラス部分の熱損失係数（U値）, W/m2K
-            r_w_o_w: 窓の室外側表面熱伝達抵抗（冬期条件）, m2K/W
-            r_w_i_w: 窓の室内側表面熱伝達抵抗（冬期条件）, m2K/W
-            r_w_o_s: 窓の室外側表面熱伝達抵抗（夏期条件）, m2K/W
-            r_w_i_s: 窓の室内側表面熱伝達抵抗（夏期条件）, m2K/W
-
-        Returns:
-            境界jの窓のガラス部分の熱損失係数（夏期条件）, W/m2K
-        """
-        return 1 / (1 / u_w_g_j - r_w_o_w - r_w_i_w + r_w_o_s + r_w_i_s)
-
 
 def _get_tau_n_phi(phi: Union[float, np.ndarray]) -> Union[float, np.ndarray]:
     """規準化透過率を計算する。
@@ -635,4 +537,108 @@ def _get_r_r_w_g_j(
         return (1 / 4 * (1 / u_w_g_j - r_w_o_w - r_w_i_w - r_w_air) + r_w_o_s) * u_w_g_s_j
     else:
         raise ValueError()
+
+
+def _get_u_w_g_s_j(u_w_g_j: float, r_w_o_w: float, r_w_i_w: float, r_w_o_s: float, r_w_i_s: float) -> float:
+    """境界jの窓のガラス部分の熱損失係数（夏期条件）を計算する。
+    Args:
+        u_w_g_j: 境界jの窓のガラス部分の熱損失係数（U値）, W/m2K
+        r_w_o_w: 窓の室外側表面熱伝達抵抗（冬期条件）, m2K/W
+        r_w_i_w: 窓の室内側表面熱伝達抵抗（冬期条件）, m2K/W
+        r_w_o_s: 窓の室外側表面熱伝達抵抗（夏期条件）, m2K/W
+        r_w_i_s: 窓の室内側表面熱伝達抵抗（夏期条件）, m2K/W
+    Returns:
+        境界jの窓のガラス部分の熱損失係数（夏期条件）, W/m2K
+    Notes:
+        eq.29
+    """
+    return 1 / (1 / u_w_g_j - r_w_o_w - r_w_i_w + r_w_o_s + r_w_i_s)
+
+
+def _get_r_w() -> Tuple[float, float, float, float]:
+    """窓の表面熱伝達抵抗を求める。
+    Returns:
+        窓の室外側表面熱伝達抵抗（冬季条件）, m2K/W
+        窓の室内側表面熱伝達抵抗（冬季条件）, m2K/W
+        窓の室外側表面熱伝達抵抗（夏季条件）, m2K/W
+        窓の室内側表面熱伝達抵抗（夏季条件）, m2K/W
+    Notes:
+        table 2
+    """
+    r_w_o_w = 0.0415
+    r_w_i_w = 0.1228
+    r_w_o_s = 0.0756
+    r_w_i_s = 0.1317
+    return r_w_o_w, r_w_i_w, r_w_o_s, r_w_i_s
+
+
+def _get_eta_w_g_j(eta_w_j: float, r_a_w_g_j: float) -> float:
+    """境界jの窓のガラス部分の日射熱取得率を取得する。
+    Args:
+        eta_w_j: 境界 j の窓の日射熱取得率
+        r_a_w_g_j: 境界 j の窓の面積に対するグレージングの面積の比, -
+    Returns:
+        境界jの窓のガラス部分の日射熱取得率, -
+    Notes:
+        eq.30
+    """
+    return eta_w_j / r_a_w_g_j
+
+
+def _get_u_w_g_j(u_w_j: float, u_w_f_j: float, r_a_w_g_j: float) -> float:
+    """境界jの窓のガラス部分の熱損失係数（U値）を取得する。
+    Args:
+        u_w_j: 境界jの窓の熱損失係数, W/m2K
+        u_w_f_j: 境界jの窓の建具部分の熱損失係数（U値）, W/m2K
+        r_a_w_g_j: 境界jの窓の面積に対するグレージングの面積の比, -
+    Returns:
+        境界jの窓のガラス部分の熱損失係数（U値）, W/m2K
+    Notes:
+        eq.31
+    """
+    return (u_w_j - u_w_f_j * (1 - r_a_w_g_j)) / r_a_w_g_j
+
+
+def _get_r_a_w_g_j(r_a_w_g_j: Optional[float], flame_type: FlameType) -> float:
+    """窓の面積に対するグレージングの面積の比が指定されていない場合に枠（フレーム）材質の種類に応じてデフォルト値を定める。
+
+    Args:
+        r_a_w_g_j: 境界jの窓の面積に対するグレージングの面積の比, -
+        flame_type: 建具（フレーム）材質の種類
+    Returns:
+        境界jの窓の面積に対するグレージングの面積の比, -
+    Notes:
+        table 3
+    """
+
+    if r_a_w_g_j is None:
+        return {
+            FlameType.RESIN: 0.72,
+            FlameType.WOOD: 0.72,
+            FlameType.ALUMINUM: 0.8,
+            FlameType.MIXED_WOOD: 0.8,
+            FlameType.MIXED_RESIN: 0.8
+        }[flame_type]
+    else:
+        return r_a_w_g_j
+
+
+def _get_u_w_f_j(flame_type: FlameType) -> float:
+    """建具部分の熱損失係数（U値）を取得する。
+    Args:
+        flame_type: 建具（フレーム）材質の種類
+    Returns:
+        境界jの窓の建具部分の熱損失係数（U値）, W/m2K
+    Notes:
+        table 4
+    """
+
+    return {
+        FlameType.RESIN: 2.2,
+        FlameType.WOOD: 2.2,
+        FlameType.ALUMINUM: 6.6,
+        FlameType.MIXED_WOOD: 4.7,
+        FlameType.MIXED_RESIN: 4.7
+    }[flame_type]
+
 
