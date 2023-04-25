a1eca837d3f4234c52a5ef138f5c980cc1bc2e1d
Hisashi Miura <miura_hisashi@hotmail.com>
2023-04-25 13:58:45 +0900

    change order of the functions and update comments

--- heat_load_calc/pmv.py
+++ heat_load_calc/pmv.py
@@ -72,7 +72,7 @@ def get_ppd_is_n(pmv_is_n: np.array) -> np.array:
     Returns:
         ステップ n における室 i の在室者のPPD, [i, 1]
     Note:
-        eq.(3)
+        eq.(2)
     """
 
     return 100.0 - 95.0 * np.exp(-0.03353 * pmv_is_n ** 4.0 - 0.2179 * pmv_is_n ** 2.0)
@@ -112,28 +112,6 @@ def get_theta_ot_target(
     f_cl_is_n = _get_f_cl_is_n(i_cl_is_n=i_cl_is_n)
 
     return _get_theta_ot_target_is_n(p_a_is_n, h_hum_is_n, pmv_target_is_n, i_cl_is_n, m_is, f_cl_is_n)
-
-def _get_theta_ot_target_is_n(p_a_is_n, h_hum_is_n, pmv_target_is_n, i_cl_is_n, m_is, f_cl_is_n):
-    """在室者の目標作用温度を計算する。
-    Calculate the target operative temperature of a occupant.
-    Args:
-        p_a_is_n: ステップ n における室 i の水蒸気圧, Pa, [i, 1]
-        h_hum_is_n: ステップ n における室 i の在室者周りの総合熱伝達率, W/m2K, [i, 1]
-        pmv_target_is_n: ステップ n における室 i の在室者の目標PMV, [i, 1]
-        i_cl_is_n: ステップ n における室 i の在室者の着衣抵抗, m2K/W, [i, 1]
-        m_is: 室 i の在室者の代謝量, W/m2, [i, 1]
-        f_cl_is_n: ステップ n における室 i の在室者の着衣面積率, [i, 1]
-    Returns:
-        ステップ n における室 i の目標作用温度, degree C, [i, 1]
-    """
-    
-    return (pmv_target_is_n / (0.303 * np.exp(-0.036 * m_is) + 0.028) - m_is
-            + 3.05 * 10 ** (-3) * (5733.0 - 6.99 * m_is - p_a_is_n)
-            + np.maximum(0.42 * (m_is - 58.15), 0.0)
-            + 1.7 * 10 ** (-5) * m_is * (5867.0 - p_a_is_n)
-            + 0.0014 * m_is * 34.0
-            + f_cl_is_n * h_hum_is_n * (35.7 - 0.028 * m_is) / (1 + i_cl_is_n * f_cl_is_n * h_hum_is_n)
-            )/(0.0014 * m_is + f_cl_is_n * h_hum_is_n / (1 + i_cl_is_n * f_cl_is_n * h_hum_is_n))
 
 
 def get_h_hum(
@@ -236,6 +214,31 @@ def _get_pmv_is_n(
             - f_cl_is_n * h_hum_is_n * (35.7 - 0.028 * m_is - theta_ot_is_n) / (1 + i_cl_is_n * f_cl_is_n * h_hum_is_n))  # 着衣からの熱損失
 
 
+def _get_theta_ot_target_is_n(p_a_is_n: np.ndarray, h_hum_is_n: np.ndarray, pmv_target_is_n: np.ndarray, i_cl_is_n: np.ndarray, m_is, f_cl_is_n: np.ndarray) -> np.ndarray:
+    """目標作用温度を計算する。
+    Calculate the target operative temperature.
+    Args:
+        p_a_is_n: ステップ n における室 i の水蒸気圧, Pa, [i, 1]
+        h_hum_is_n: ステップ n における室 i の在室者周りの総合熱伝達率, W/m2K, [i, 1]
+        pmv_target_is_n: ステップ n における室 i の在室者の目標PMV, [i, 1]
+        i_cl_is_n: ステップ n における室 i の在室者の着衣抵抗, m2K/W, [i, 1]
+        m_is: 室 i の在室者の代謝量, W/m2, [i, 1]
+        f_cl_is_n: ステップ n における室 i の在室者の着衣面積率, [i, 1]
+    Returns:
+        ステップ n における室 i の目標作用温度, degree C, [i, 1]
+    NOte:
+        eq.(3)
+    """
+    
+    return (pmv_target_is_n / (0.303 * np.exp(-0.036 * m_is) + 0.028) - m_is
+            + 3.05 * 10 ** (-3) * (5733.0 - 6.99 * m_is - p_a_is_n)
+            + np.maximum(0.42 * (m_is - 58.15), 0.0)
+            + 1.7 * 10 ** (-5) * m_is * (5867.0 - p_a_is_n)
+            + 0.0014 * m_is * 34.0
+            + f_cl_is_n * h_hum_is_n * (35.7 - 0.028 * m_is) / (1 + i_cl_is_n * f_cl_is_n * h_hum_is_n)
+            )/(0.0014 * m_is + f_cl_is_n * h_hum_is_n / (1 + i_cl_is_n * f_cl_is_n * h_hum_is_n))
+
+
 def _get_theta_ot_is_n(h_hum_r_is_n: np.ndarray, theta_mrt_is_n: np.ndarray, h_hum_c_is_n: np.ndarray, theta_r_is_n: np.ndarray, h_hum_is_n: np.ndarray) -> np.ndarray:
     """在室者の作用温度を計算する。
     Calculate the operative temperature of the occupant.
@@ -248,11 +251,10 @@ def _get_theta_ot_is_n(h_hum_r_is_n: np.ndarray, theta_mrt_is_n: np.ndarray, h_h
     Returns:
         ステップ n における室 i の在室者の作用温度, degree C, [i, 1]
     Notes:
-        eq.(2)
+        eq.(5)
     """
 
     return (h_hum_r_is_n * theta_mrt_is_n + h_hum_c_is_n * theta_r_is_n) / h_hum_is_n
-
 
 
 def _get_theta_cl_is_n(
