bb8a410271ad38efbf0645cbe76f2616863a2f66
Hisashi Miura <hisashi.office@gmail.com>
2024-05-31 15:27:13 +0900

    make f_wsr property

--- heat_load_calc/sequence.py
+++ heat_load_calc/sequence.py
@@ -25,9 +25,6 @@ from heat_load_calc.operation_mode import Operation, OperationMode
 @dataclass
 class PreCalcParameters:
 
-    # WSR, WSB の計算 式(24)
-    f_wsr_js_is: np.ndarray
-
     # WSC, W, [j, n]
     f_wsc_js_ns: np.ndarray
 
@@ -216,7 +213,6 @@ class Sequence:
         )
 
         pre_calc_parameters = PreCalcParameters(
-            f_wsr_js_is=f_wsr_js_is,
             f_wsc_js_ns=f_wsc_js_ns,
             k_r_is_n=k_r_is_n,
             k_c_is_n=k_c_is_n,
@@ -281,6 +277,9 @@ class Sequence:
 
         # the shape factor of boundaries j for the microsphier in the room i, [i, j]
         self._f_mrt_is_js = f_mrt_is_js
+
+        # f_WSR, -, [J, I]
+        self._f_wsr_js_is = f_wsr_js_is
 
         self._pre_calc_parameters = pre_calc_parameters
 
@@ -367,6 +366,11 @@ class Sequence:
     def f_mrt_is_js(self):
         """the shape factor of boundaries j for the microsphier in the room i, [i, j]"""
         return self._f_mrt_is_js
+    
+    @property
+    def f_wsr_js_is(self):
+        """f_WSR, -, [J, I]"""
+        return self._f_wsr_js_is
 
     @property
     def pre_calc_parameter(self):
@@ -476,7 +480,7 @@ class Sequence:
             v_rm_is=self.rms.v_r_is,
             c_sh_frt_is=self.rms.c_sh_frt_is,
             delta_t=delta_t,
-            f_wsr_js_is=ss.f_wsr_js_is,
+            f_wsr_js_is=self.f_wsr_js_is,
             g_sh_frt_is=self.rms.g_sh_frt_is,
             h_s_c_js=self.bs.h_s_c_js,
             p_is_js=self.bs.p_is_js,
@@ -554,8 +558,8 @@ class Sequence:
         theta_r_ntr_non_nv_is_n_pls = np.dot(ss.f_xot_is_is_n_pls, theta_r_ot_ntr_non_nv_is_n_pls) - f_xc_is_n_pls
         theta_r_ntr_nv_is_n_pls = np.dot(ss.f_xot_is_is_n_pls, theta_r_ot_ntr_nv_is_n_pls) - f_xc_is_n_pls
 
-        theta_s_ntr_non_nv_js_n_pls = np.dot(ss.f_wsr_js_is, theta_r_ntr_non_nv_is_n_pls) + ss.f_wsc_js_ns[:, n + 1].reshape(-1, 1) + f_wsv_js_n_pls
-        theta_s_ntr_nv_js_n_pls = np.dot(ss.f_wsr_js_is, theta_r_ntr_nv_is_n_pls) + ss.f_wsc_js_ns[:, n + 1].reshape(-1, 1) + f_wsv_js_n_pls
+        theta_s_ntr_non_nv_js_n_pls = np.dot(self.f_wsr_js_is, theta_r_ntr_non_nv_is_n_pls) + ss.f_wsc_js_ns[:, n + 1].reshape(-1, 1) + f_wsv_js_n_pls
+        theta_s_ntr_nv_js_n_pls = np.dot(self.f_wsr_js_is, theta_r_ntr_nv_is_n_pls) + ss.f_wsc_js_ns[:, n + 1].reshape(-1, 1) + f_wsv_js_n_pls
 
         theta_mrt_hum_ntr_non_nv_is_n_pls = np.dot(self.f_mrt_is_js, theta_s_ntr_non_nv_js_n_pls)
         theta_mrt_hum_ntr_nv_is_n_pls = np.dot(self.f_mrt_is_js, theta_s_ntr_nv_js_n_pls)
@@ -745,7 +749,7 @@ class Sequence:
         theta_s_js_n_pls = get_theta_s_js_n_pls(
             f_wsb_js_is_n_pls=f_wsb_js_is_n_pls,
             f_wsc_js_n_pls=ss.f_wsc_js_ns[:, n + 1].reshape(-1, 1),
-            f_wsr_js_is=ss.f_wsr_js_is,
+            f_wsr_js_is=self.f_wsr_js_is,
             f_wsv_js_n_pls=f_wsv_js_n_pls,
             l_rs_is_n=l_rs_is_n,
             theta_r_is_n_pls=theta_r_is_n_pls
