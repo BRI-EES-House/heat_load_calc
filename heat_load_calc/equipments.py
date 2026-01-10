fix wrong index

heat_load_calc/equipments.py
@@ -175,9 +175,9 @@ class Individual:
     def get_f_l_cl2(self, q_s_is_n: np.ndarray, theta_r_is_n_pls: np.ndarray, x_r_ntr_is_n_pls: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
 
         # ここでidをひいているのは間違い
-        q_s_i_n = q_s_is_n[self.room_id, 0]
-        theta_r_i_n_pls = theta_r_is_n_pls[self.room_id, 0]
-        x_r_ntr_i_n_pls = x_r_ntr_is_n_pls[self.room_id, 0]
+        q_s_i_n = q_s_is_n[self.room_index, 0]
+        theta_r_i_n_pls = theta_r_is_n_pls[self.room_index, 0]
+        x_r_ntr_i_n_pls = x_r_ntr_is_n_pls[self.room_index, 0]
 
         brmx_rac_is, brcx_rac_is = self.e.get_f_l_cl(q_s=q_s_i_n, theta_r=theta_r_i_n_pls, x_r=x_r_ntr_i_n_pls)
 
@@ -185,8 +185,8 @@ class Individual:
         brxc_is = np.zeros((self.n_rm, 1), dtype=float)
 
         # ここでidをひいているのは間違い
-        brmx_is_is[self.room_id, self.room_id] = brmx_rac_is
-        brxc_is[self.room_id, 0] = brcx_rac_is
+        brmx_is_is[self.room_index, self.room_index] = brmx_rac_is
+        brxc_is[self.room_index, 0] = brcx_rac_is
 
         return brmx_is_is, brxc_is
         